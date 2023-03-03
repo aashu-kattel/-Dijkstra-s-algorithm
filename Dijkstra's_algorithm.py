309d4c050c7a89871c132bb78b6bca501d23720c
Aashutosh Kattel <90999891+aashu-kattel@users.noreply.github.com>
2023-03-03 16:21:13 +0545

    Create  Dijkstra's_algorithm.py

---
+++ Dijkstra's_algorithm.py
@@ -0,0 +1,27 @@
+import heapq
+
+def dijkstra(graph, start):
+    # Initialize distances to all vertices as infinity except for the start vertex
+    distances = {vertex: float('infinity') for vertex in graph}
+    distances[start] = 0
+
+    # Use a priority queue to keep track of the vertices with the smallest distances
+    pq = [(0, start)]
+
+    while len(pq) > 0:
+        # Pop the vertex with the smallest distance from the priority queue
+        current_distance, current_vertex = heapq.heappop(pq)
+
+        # If the distance to the current vertex is already larger than the stored distance, skip
+        if current_distance > distances[current_vertex]:
+            continue
+
+        # Iterate over the neighbors of the current vertex and update their distances if needed
+        for neighbor, weight in graph[current_vertex].items():
+            distance = current_distance + weight
+
+            if distance < distances[neighbor]:
+                distances[neighbor] = distance
+                heapq.heappush(pq, (distance, neighbor))
+
+    return distances
